c62d5a510d5f720475565469c64e131049904dfa
jmndz <janaemilyn@gmail.com>
2022-08-31 19:30:13 +0800

    Added icons and background color to indicate invalid or valid input

--- script.js
+++ script.js
@@ -5,7 +5,13 @@ const songList = ['./audio/ENHYPEN - Blessed Cursed.mp3',
                   './audio/ENHYPEN - Given-Taken「Audio」.mp3',
                   './audio/ENHYPEN - ParadoXXX Invasion.mp3',
                   './audio/ENHYPEN - Tamed-Dashed「Audio」.mp3',
-                  './audio/ENHYPEN (엔하이픈) - Future Perfect (Pass the MIC) 「Audio」.mp3'];
+                  './audio/ENHYPEN (엔하이픈) - Future Perfect (Pass the MIC) 「Audio」.mp3',
+                  './audio/ENHYPEN - SHOUT OUT.mp3',
+                  './audio/ENHYPEN - TFW (That Feeling When).mp3',
+                  './audio/ENHYPEN - Forget Me Not.mp3',
+                  './audio/ENHYPEN - I Need The Light.mp3',
+                  './audio/ENHYPEN - Polaroid Love.mp3',
+                  './audio/ENHYPEN (엔하이픈) - Always 「Audio」.mp3'];
 const song = new Audio();
 audio.addEventListener('click', playAudio);
 
